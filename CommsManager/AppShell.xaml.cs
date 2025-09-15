0c5246113b16f8a2bd35c506b5288b1dfafbcf16
MaxGog <max.gog2005@outlook.com>
2025-09-15 17:02:02 +0300

    Initial commit

---
+++ CommsManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CommsManager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CommsManager/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommsManager")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommsManager.Pages")]
